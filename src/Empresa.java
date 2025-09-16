5b1d5329bcc24956d4557cd774d64ca045e85ec1
Andres <andydquintero@gmail.com>
2025-09-15 20:53:34 -0500

    AddedBusinessClass

---
+++ src/Empresa.java
@@ -0,0 +1,8 @@
+public class Empresa {
+    double nivelDeRiesgo = 0.0;
+    double monto = 0.0;
+    int plazo = 0;
+    double porcentajeRetorno = 0;
+void calcularRetorno(){double retorno = monto * (porcentajeRetorno*100);
+System.out.println(retorno);}
+}
